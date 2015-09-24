fcbcce1b7033591cd5c01c696d7f17f3fc1c2665
Philip Dai <smartielion@gmail.com>
2015-09-24 02:11:58 -0500

    Binding data

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ weatherApp.config(function($routeProvider) {
 		.when('/forecast', {
 			templateUrl: 'pages/forecast.htm',
 			controller: 'forecastController'
+		})
+		.when('/forecast/:days', {
+			templateUrl: 'pages/forecast.htm',
+			controller: 'forecastController'
 		});
 });
 
@@ -24,6 +28,19 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 	});
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 	$scope.city = cityService.city;
+	$scope.days = $routeParams.days || 2;
+	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", 
+																{callback: "JSON_CALLBACK"}, 
+																{get: { method: "JSONP" }});
+	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+	
+	$scope.convertToFahrenheit = function(degK) {
+		return Math.round(degK - 273.15);
+	};
+	
+	$scope.convertToDate = function(dt) {
+		return new Date(dt * 1000);
+	};
 }]);
