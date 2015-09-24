e17b61bb6008094134f105f6f1c39e2912e3c23d
Philip Dai <smartielion@gmail.com>
2015-09-24 01:11:21 -0500

    Build up the backborn for the whole single page project

---
+++ app.js
@@ -0,0 +1,29 @@
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/home.htm',
+			controller: 'homeController'
+		})
+		.when('/forecast', {
+			templateUrl: 'pages/forecast.htm',
+			controller: 'forecastController'
+		});
+});
+
+weatherApp.service('cityService', function() {
+	this.city = "New York, NY";
+});
+
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+	$scope.city = cityService.city;
+	
+	$scope.$watch('city', function() {
+		cityService.city = $scope.city;
+	});
+}]);
+
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+	$scope.city = cityService.city;
+}]);
